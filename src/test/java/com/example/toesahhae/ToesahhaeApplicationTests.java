41480dfd9a2449d65be58e1d1deb97bd8ed53d18
99winnmin <ojysep9987@gmail.com>
2023-09-19 00:57:55 +0900

    :tada: Feat : Project Init

---
+++ src/test/java/com/example/toesahhae/ToesahhaeApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.toesahhae;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ToesahhaeApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
